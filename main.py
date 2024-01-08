f709fd008e7a8a8ad872ea3ce8f86974347a5a92
Archolic95 <54243064+Archolic95@users.noreply.github.com>
2024-01-07 18:06:19 -0800

    fix the backend base 64 decoding issue
    For some reason, when generating the base64 string, it includes a byte decorator b'{base64}', causing the image reading to fail

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from io import BytesIO
 from datetime import datetime
 from pydantic import BaseModel
 import uvicorn
+import json
+from PIL import Image
 
 app = FastAPI()
 pipe = AutoPipelineForInpainting.from_pretrained("stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16").to("cuda")
@@ -28,16 +30,25 @@ async def root():
     return {"message": "Hello World"}
 
 @app.post("/outpainting")
-async def generate():
-    print('request received')
+async def generate(requsetBody: Request):
+    requestBodyJsonString = await requsetBody.json()
+    requestBodyJsonDict = json.loads(requestBodyJsonString)
+    print('request received, content of body:', requestBodyJsonDict['image'])
     img_url = "https://raw.githubusercontent.com/CompVis/latent-diffusion/main/data/inpainting_examples/overture-creations-5sI6fQgYIuo.png"
     mask_url = "https://raw.githubusercontent.com/CompVis/latent-diffusion/main/data/inpainting_examples/overture-creations-5sI6fQgYIuo_mask.png"
     
     image = load_image(img_url).resize((1024, 1024))
     mask_image = load_image(mask_url).resize((1024, 1024))
-    
-    prompt = "a tiger sitting on a park bench"
-    #prompt = requsetBody.prompt
+
+    #prompt = "a tiger sitting on a park bench"
+    prompt = requestBodyJsonDict['prompt']
+    base64image = requestBodyJsonDict['image'][2:-1]
+    # Convert back to image
+    #image = retrieve_image(requestBodyJsonDict['image'])
+    image = retriev_image_file(base64image)
+
+    image = image.resize((1024, 1024))
+
     generator = torch.Generator(device="cuda").manual_seed(0)
 
     image = pipe(
@@ -62,5 +73,36 @@ async def generate():
 
     return {"image": req_file}
 
+def retriev_image_file(encoded_data):
+    image_byte = base64.b64decode(encoded_data)
+    file_name = 'input_image.jpg'
+    with open(file_name, 'wb') as f:
+        f.write(image_byte)
+    
+    #image = cv2.imread(file_name)
+    image = Image.open(file_name)
+    return image
+
+def retrieve_image(encoded_data):
+    nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_UNCHANGED)
+    print(img.shape)
+    return img
+
+def retriev_image_data(encoded_data):
+    input_image = base64.b64decode(encoded_data)
+    jpg_as_np = np.frombuffer(input_image, dtype=np.uint8)
+    img = cv2.imdecode(jpg_as_np, flags=1)
+    print(img.shape)
+    return img
+
+
+def add_image_to_canvas():
+
+    return
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=30000)
+    # Need to use the import string style in order to run reload; change local host to 0.0.0.0 to openup
+    #uvicorn.run("main:app", host="127.0.0.1", port=30000, reload = True, workers=1)
+    uvicorn.run(app, host="127.0.0.1", port=8001)
+    #uvicorn.run(app, host="0.0.0.0", port=30000)
